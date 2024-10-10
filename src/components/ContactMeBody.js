4467f0f870b764098eedbde28f139b83c2655e15
Arpit Aggarwal <117593103+arpitaggarwal0511@users.noreply.github.com>
2024-10-10 18:05:20 +0530

    feat : contactme + preoject body

--- src/components/ContactMeBody.js
+++ src/components/ContactMeBody.js
@@ -1,9 +1,26 @@
+import linkedicon from "../utils/liknedin.png"
+import githubicon from "../utils/githubicon.png"
+import resumeimg from "../utils/resume.jpg"
 const ResumeContact = () => {
-    
-    return (
-        <div className="contactmebody">
-            Contact Me here
-        </div>
-    );
+  return (
+    <div className="contactmebody">
+      <h1>Let's Connect!</h1>
+      <h3>You can reach me at:</h3>
+      <div
+        className="emaillink"
+        onClick={() =>
+          (window.location.href = "mailto:arpitaggarwal0511@gmail.com")
+        }
+      >
+        arpitaggarwal0511@gmail.com
+      </div>
+      <div className="conticons">
+      <img onClick={()=> window.open('https://www.linkedin.com/in/arpit-aggarwal-5b6040257/')} src={linkedicon}></img>
+      <img onClick={()=> window.open('https://github.com/arpitaggarwal0511N')} src={githubicon}></img>
+      </div>
+      <img onClick={()=>window.open('https://drive.google.com/file/d/1jnB4bsiscMeC0XoIkIp_HXGHCpc6BsI8/view')} className="resumeimage" src={resumeimg}></img>
+        
+    </div>
+  );
 };
 export default ResumeContact;
--- src/components/ProjectsBody.js
+++ src/components/ProjectsBody.js
@@ -1,5 +1,8 @@
 import devimg from "../utils/pccat.png"
 import food from "../utils/foodcat.png"
+import catfact from "../utils/catfact.png"
+import load from "../utils/loading.png"
+import Link from "react-router-dom";
 const Projects = () => {
     
     return (
@@ -13,9 +16,22 @@ const Projects = () => {
             </h3>
             <div className="cardsholder">
             <div className="project-cards">
-                <div className="card">
+
+                <div className="card" 
+                 onClick={() => window.open('https://github.com/arpitaggarwal0511/Food-Delivery-app')}>
                 <img className="card-icons" src={food}/>                
                     <p>Food Delivery Web Application</p>
+                </div>
+
+
+                <div className="card"
+                onClick={() => window.open('https://randomkittyfacts.netlify.app/')}>
+                <img className="card-icons" src={catfact}/>                
+                    <p>Random Cat Fact Generator</p>
+                </div>
+                <div className="card">
+                <img className="card-icons" src={load}/>                
+                    <p>More Coming Soon !!</p>
                 </div>
                 
             </div>
